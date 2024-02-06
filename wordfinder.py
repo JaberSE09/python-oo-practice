fc907a2e3b774ef6b2f6fde5eb705a447240d7ba
Sajid Jaber <sajidjaber@yahoo.com>
2024-02-06 13:09:37 -0600

    finished the word finder

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,20 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    def __init__(self,filename):
+        self.filename = open(filename, "r")
+        self.lines=self.filename.readlines()
+
+    def getReadWords(self):
+        count = 0        
+            
+        return print(str(len(self.lines)) + " words read" )
+    def random(self):
+        length = len(self.lines)
+        randomNum= random.randint(0, length)
+        return print(self.lines[randomNum])
+words= WordFinder("words.txt") 
+words.getReadWords()
+words.random()
+
