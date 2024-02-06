a5edc70e5e5ef7d1aec10cf3ae41cf7b161cbcd3
Sajid Jaber <sajidjaber@yahoo.com>
2024-02-06 11:25:40 -0600

    serial setup

--- serial.py
+++ serial.py
@@ -1,6 +1,16 @@
 """Python serial number generator."""
 
 class SerialGenerator:
+    def __init__(self, START):
+        self.START=START
+        self.increment=START
+       
+    def generate(self):
+        print(self.increment)
+        self.increment= self.increment + 1
+    def reset(self):
+        self.increment=self.START
+       
     """Machine to create unique incrementing serial numbers.
     
     >>> serial = SerialGenerator(start=100)
@@ -19,4 +29,12 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+serial = SerialGenerator(START=100)
+serial.generate()
+serial.generate()
+serial.generate()
+serial.reset()
+serial.generate()
+    
 
